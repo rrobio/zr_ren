8e1a20cf56fa25d8ae71bb33605d2d12095f94cf
rrobio <orlandinirobi@gmail.com>
2022-09-03 19:04:44 +0200

    Camera: fix vector updating and refactor

--- src/camera.hpp
+++ src/camera.hpp
@@ -12,21 +12,21 @@ namespace ren {
 
 class Camera {
 public:
-  Camera(glm::vec3 look_from, glm::vec3 look_at, glm::vec3 vup, float vfov,
+  Camera(vec3 look_from, point3 look_at, vec3 vup, float vfov,
          float const aspect_ratio)
-      : m_position(look_from), m_up(vup), m_world_up(vup) {
+      : m_position(look_from), m_up(vup), m_world_up(vup), m_fov(vfov) {
     auto theta = glm::radians(vfov);
     auto h = std::tan(theta / 2);
-    float viewport_height = 2.0 * h;
-    float viewport_width = aspect_ratio * viewport_height;
+    m_viewport_height = 2.0 * h;
+    m_viewport_width = aspect_ratio * m_viewport_height;
 
     auto w = glm::normalize(look_from - look_at);
     auto u = glm::normalize(cross(vup, w));
     auto v = cross(w, u);
 
     origin = look_from;
-    horizontal = viewport_width * u;
-    vertical = viewport_height * v;
+    horizontal = m_viewport_width * u;
+    vertical = m_viewport_height * v;
     lower_left_corner = origin - horizontal / 2.0f - vertical / 2.0f - w;
     update_vectors();
   }
@@ -34,6 +34,9 @@ public:
   auto view() const {
     return glm::lookAt(m_position, m_position + m_front, m_up);
   }
+  
+  auto fov() const { return m_fov; }
+  auto set_fov(float f) { m_fov = f; }
 
   ray get_ray(float s, float t) const {
     return ray(origin,
@@ -73,35 +76,27 @@ public:
   }
 
   void update_rt_vectors() {
-    update_vectors();
-    auto theta = glm::radians(90.f);
-    auto h = std::tan(theta / 2);
-    float viewport_height = 2.0 * h;
-    float viewport_width = 16.f / 9.f * viewport_height;
-
-    auto look_from = m_position;
-    auto look_at = m_front;
-    auto vup = m_up;
-
-    auto w = glm::normalize(look_from - look_at);
-    auto u = glm::normalize(cross(vup, w));
+    auto w = glm::normalize(-m_front);
+    auto u = glm::normalize(cross(m_up, w));
     auto v = cross(w, u);
 
-    origin = look_from;
-    horizontal = viewport_width * u;
-    vertical = viewport_height * v;
+    origin = m_position;
+    horizontal = m_viewport_width * u;
+    vertical = m_viewport_height * v;
     lower_left_corner = origin - horizontal / 2.0f - vertical / 2.0f - w;
   }
 
 private:
+  float m_fov;
+  
   float m_yaw{-90.0f};
   float m_pitch{0.0f};
 
-  glm::vec3 m_position{};
-  glm::vec3 m_front{};
-  glm::vec3 m_up{};
-  glm::vec3 m_right{};
-  glm::vec3 m_world_up{};
+  point3 m_position{};
+  vec3 m_front{};
+  vec3 m_up{};
+  vec3 m_right{};
+  vec3 m_world_up{};
 
   double m_last_x{};
   double m_last_y{};
@@ -111,6 +106,9 @@ private:
   vec3 horizontal;
   vec3 vertical;
   // vec3 u, v, w;
+  
+  float m_viewport_height;
+  float m_viewport_width;
 
   void update_vectors() {
     glm::vec3 front;
