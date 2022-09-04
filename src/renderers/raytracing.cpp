37c78a211805bda9c4198408259b4ee417a1714c
rrobio <orlandinirobi@gmail.com>
2022-09-04 18:01:14 +0200

    rt: big (still wip) update for realtime rendering

--- src/renderers/raytracing.cpp
+++ src/renderers/raytracing.cpp
@@ -16,67 +16,7 @@
 #include "stb_image_write.h"
 
 namespace ren {
-RayTracingRenderer::RayTracingRenderer(std::filesystem::path root_dir) {
-  auto ren_material_ground = std::make_shared<ren::Material>(
-      std::make_shared<ren::lambertian>(color(0.8, 0.8, 0.0)));
-  auto ren_material_center = std::make_shared<ren::Material>(
-      std::make_shared<ren::lambertian>(color(0.1, 0.2, 0.5)));
-  auto ren_material_left =
-      std::make_shared<ren::Material>(std::make_shared<ren::dielectric>(1.5));
-  auto ren_material_right = std::make_shared<ren::Material>(
-      std::make_shared<ren::metal>(color(0.8, 0.6, 0.2), 0.0));
-
-  m_scene.add_object(
-      ren::create_sphere(point3(0.0, -100.5, -1.0), 100, ren_material_ground));
-  m_scene.add_object(
-      ren::create_sphere(point3(0.0, 0.0, -1.0), 0.5, ren_material_center));
-  m_scene.add_object(
-      ren::create_sphere(point3(-1.0, 0.0, -1.0), 0.5, ren_material_left));
-  m_scene.add_object(
-      ren::create_sphere(point3(-1.0, 0.0, -1.0), -0.45, ren_material_left));
-  m_scene.add_object(
-      ren::create_sphere(point3(1.0, 0.0, -1.0), 0.5, ren_material_right));
-}
-
-void RayTracingRenderer::render(const Scene &scene,
-                                Transformations const &trans, double ticks) {
-
-  assert(trans.cam);
-  if (!a_camera) a_camera = trans.cam;
-  if (m_rendering) {
-    if (is_render_done()) {
-      create_image_data();
-      m_rendering = false;
-      m_has_render = true;
-      thread_cleanup();
-    }
-  }
-}
-
-void RayTracingRenderer::draw_dialog() {
-  ImGui::Text("RayTracing");
-  if (!m_rendering) {
-    if (ImGui::Button("Render Frame")) {
-      render_frame(&m_scene);
-    }
-  }
-
-  ImGui::InputInt("Number of threads", &m_n_threads);
-  ImGui::InputInt("Max Depth", &m_max_depth);
-  ImGui::InputInt("Samples Per Pixle", &m_samples_per_pixel);
-
-  if (m_has_render) {
-    if (ImGui::Button("Save to file")) {
-      save_to_file();
-    }
-    ImGui::Begin("Rendered Frame");
-    m_texture.bind();
-    ImGui::Image((ImTextureID)(intptr_t)m_texture.id,
-                 ImVec2(m_texture.width, m_texture.height));
-    ImGui::End();
-  }
-}
-
+// raytracing
 static bool hit_scene(ray const &r, Scene const *world, int depth,
                       hit_record &rec) {
 
@@ -96,6 +36,17 @@ static bool hit_scene(ray const &r, Scene const *world, int depth,
       rec = temp_rec;
     }
   }
+  for (auto const &object : world->lights()) {
+    if (!object.hit()) {
+      continue;
+    }
+    if (object.hit()(object, r, t_min, closest_so_far, temp_rec)) { // malo
+                                                                    // ruzno
+      hit_anything = true;
+      closest_so_far = temp_rec.t;
+      rec = temp_rec;
+    }
+  }
   return hit_anything;
 }
 
@@ -105,99 +56,368 @@ static color ren_ray_color(ray const &r, Scene const *world, int depth) {
   if (depth <= 0)
     return color(0, 0, 0);
 
-  if (hit_scene(r, world, depth, rec)) {
-    // point3 target = rec.p + rec.normal + random_in_unit_sphere();
-    // point3 target = rec.p + rec.normal +
-    // random_in_hemisphere(rec.normal); point3 target = rec.p + rec.normal
-    // + random_unit_vector(); return 0.5 * ray_color(ray(rec.p, target -
-    // rec.p), world, depth-1);
-    ray scattered;
-    color attenuation;
-    if (rec.mat_ptr->scatter->scatter(r, rec, attenuation, scattered))
-      return attenuation * ren_ray_color(scattered, world, depth - 1);
-    return color(0, 0, 0);
+  if (!hit_scene(r, world, depth, rec)) {
+    return color(0.2f, 0.2f, 0.2f);
   }
-  vec3 unit_direction = glm::normalize(r.direction());
-  float t = 0.5 * (unit_direction.y + 1.0);
-  return (1.0f - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+
+  ray scattered;
+  color attenuation;
+  color emitted = rec.mat_ptr->scatter->emitted();
+
+  if (!rec.mat_ptr->scatter->scatter(r, rec, attenuation, scattered))
+    return emitted;
+  
+    
+  return emitted + attenuation * ren_ray_color(scattered, world, depth - 1);
+
+  // vec3 unit_direction = glm::normalize(r.direction());
+  // float t = 0.5 * (unit_direction.y + 1.0);
+  // return (1.0f - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-struct RenderTaskArgs {
-  std::shared_ptr<Camera> cam;
-  size_t image_height;
-  size_t image_width;
-  int samples_per_pixel;
-  int max_depth;
-  int start;
-  int stop;
-};
-
-void ren_task(RenderTaskArgs ra, Scene const *scene, Pixels *pixels) {
+void ren_task(RayTracingRenderer::ThreadTask task,
+              RayTracingRenderer::RenderTaskArgs ra, Scene const *scene) {
   auto image_height = ra.image_height;
   auto image_width = ra.image_width;
-  size_t const len = image_height * image_width * 3;
+  int const len = image_height * image_width * 3;
   int samples_per_pixel = ra.samples_per_pixel;
   int max_depth = ra.max_depth;
-  std::vector<uint8_t> apixels;
   int index = ra.start * image_width * 3;
-  // pixels.resize(len);
-  // for (int j = ra.stop; j >= ra.start; --j) {
-  for (int j = ra.start; j <= ra.stop; ++j) {
-    for (int i = 0; i < image_width; ++i) {
-      color pixel_color(0, 0, 0);
-      for (int s = 0; s < samples_per_pixel; ++s) {
-        auto u = (i + random_float()) / (image_width - 1);
-        auto v = (j + random_float()) / (image_height - 1);
-        ray r = ra.cam->get_ray(u, v);
-        pixel_color += ren_ray_color(r, scene, max_depth);
+
+  assert(task.pixels);
+  Pixels *pixels = task.pixels;
+
+  auto type = task.type;
+  do {
+    if (type == RayTracingRenderer::ThreadTaskType::realtime) {
+      ra.cam->update_rt_vectors();
+      if (*(task.should_finish)) {
+        return;
       }
-      auto [x, y, z] = get_pixel_tuple(pixel_color, samples_per_pixel);
-      (*pixels).at(index++) = x;
-      (*pixels).at(index++) = y;
-      (*pixels).at(index++) = z;
+      if (*task.thread_finished || *task.should_pause) {
+        std::this_thread::yield();
+        continue;
+      }
     }
-  }
-  return;
+    for (int j = ra.start; j <= ra.stop; ++j) {
+      for (int i = 0; i < image_width; ++i) {
+        if (type == RayTracingRenderer::ThreadTaskType::realtime) {
+          if (*task.should_finish) {
+            return;
+          }
+          if (*task.should_pause) {
+            do {
+              std::this_thread::yield();
+            } while (*task.should_pause);
+          }
+        }
+        color pixel_color(0, 0, 0);
+        for (int s = 0; s < samples_per_pixel; ++s) {
+          auto u = (i + random_float()) / (image_width - 1);
+          auto v = (j + random_float()) / (image_height - 1);
+          ray r = ra.cam->get_ray(u, v);
+          //  debug
+          // for (auto &obj : scene->objects()) {
+          //   auto objt = obj.translation();
+          //   std::printf("obj: pos(%f %f %f), scale(%f)\n", objt.x, objt.y,
+          //               objt.z, obj.scale().x);
+          // }
+          // auto cpos = ra.cam->pos();
+          // std::printf("cam: pos(%f %f %f), front(%f, %f, %f)\n", cpos.x,
+          // cpos.y,
+          //             cpos.z, 0.f, 0.f, 0.f);
+          // /debug
+          pixel_color += ren_ray_color(r, scene, max_depth);
+        }
+        auto [x, y, z] = get_pixel_tuple(pixel_color, samples_per_pixel);
+        (*pixels).at(index++) = x;
+        (*pixels).at(index++) = y;
+        (*pixels).at(index++) = z;
+      }
+    }
+    index = 0;
+    *task.thread_finished = true;
+  } while (task.type == RayTracingRenderer::ThreadTaskType::realtime);
 };
 
+RayTracingRenderer::RayTracingRenderer(std::filesystem::path root_dir) {
+  m_fstexture_shader = ren::Shader(root_dir / "shaders/fstexture.vert",
+                                   root_dir / "shaders/fstexture.frag");
+  assert(m_fstexture_shader.success());
+
+  glGenFramebuffers(1, &m_framebuffer);
+  glBindFramebuffer(GL_FRAMEBUFFER, m_framebuffer);
+
+  float vertices[] = {
+      // positions        // texture coords
+      1.f,  1.f,  0.0f, 1.0f, 1.0f, // top right
+      1.f,  -1.f, 0.0f, 1.0f, 0.0f, // bottom right
+      -1.f, -1.f, 0.0f, 0.0f, 0.0f, // bottom left
+      -1.f, 1.f,  0.0f, 0.0f, 1.0f  // top left
+  };
+  unsigned int indices[] = {
+      0, 1, 3, // first triangle
+      1, 2, 3  // second triangle
+  };
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  // texture coord attribute
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // auto ren_material_ground = std::make_shared<ren::Material>(
+  //     std::make_shared<ren::lambertian>(color(0.8, 0.8, 0.0)));
+  // auto ren_material_center = std::make_shared<ren::Material>(
+  //     std::make_shared<ren::lambertian>(color(0.1, 0.2, 0.5)));
+  // auto ren_material_left =
+  //     std::make_shared<ren::Material>(std::make_shared<ren::dielectric>(1.5));
+  // auto ren_material_right = std::make_shared<ren::Material>(
+  //     std::make_shared<ren::metal>(color(0.8, 0.6, 0.2), 0.0));
+
+  // m_scene.add_object(
+  //     ren::create_sphere(point3(0.0, -100.5, -1.0), 100,
+  //     ren_material_ground));
+  // m_scene.add_object(
+  //     ren::create_sphere(point3(0.0, 0.0, -1.0), 0.5, ren_material_center));
+  // m_scene.add_object(
+  //     ren::create_sphere(point3(-1.0, 0.0, -1.0), 0.5, ren_material_left));
+  // m_scene.add_object(
+  //     ren::create_sphere(point3(-1.0, 0.0, -1.0), -0.45, ren_material_left));
+  // m_scene.add_object(
+  //     ren::create_sphere(point3(1.0, 0.0, -1.0), 0.5, ren_material_right));
+}
+
+void RayTracingRenderer::render(const Scene &scene,
+                                Transformations const &trans, double ticks) {
+
+  assert(trans.cam);
+  a_camera = trans.cam;
+  a_scene = &scene;
+  // std::cout << "start ---";
+  // for (auto &obj : a_scene->objects()) {
+  //   std::printf("obj %f %f\n", obj.translation().x, obj.scale().x);
+  // }
+  // std::cout << "stop ---";
+  if (m_rendering) {
+    if (is_render_done()) {
+      create_image_data();
+      m_rendering = false;
+      m_has_render = true;
+      thread_cleanup();
+    }
+  }
+  if (m_render_realtime) {
+    if (realtime_threads_finished()) {
+      rt_pause();
+      rt_create_image_data();
+      rt_unpause();
+    }
+  }
+
+  // realtime rendering
+  if (m_has_render || m_render_realtime) {
+    m_fstexture_shader.use();
+    if (m_render_realtime) {
+      m_realtime_texture.bind(GL_TEXTURE0);
+    } else {
+      m_texture.bind(GL_TEXTURE0);
+    }
+
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+  }
+}
+
+void RayTracingRenderer::setup_realtime() {
+  if (m_realtime_setup)
+    return;
+
+  m_realtime_threads.clear();
+  for (auto &b : m_realtime_thread_finished) {
+    b = false;
+  }
+  for (auto &t : m_realtime_thread_should_stop) {
+    t = false;
+  }
+  m_should_pause = false;
+
+  m_realtime_pixels.resize(m_len);
+
+  setup_threads(
+      {ThreadTaskType::realtime, &m_realtime_pixels, 0, nullptr, nullptr},
+      a_scene);
+
+  Log::the().add_log("Realtime setup\n");
+  m_realtime_setup = true;
+}
+void RayTracingRenderer::destroy_realtime() {
+  if (!m_realtime_setup)
+    return;
+
+  for (auto &t : m_realtime_thread_finished) {
+    t = false;
+  }
+  for (auto &t : m_realtime_thread_should_stop) {
+    t = true;
+  }
+  m_should_pause = false;
+  for (auto &t : m_realtime_threads) {
+    t.join();
+  }
+
+  m_realtime_pixels.clear();
+  m_realtime_threads.clear();
+
+  Log::the().add_log("Realtime destroyed\n");
+  m_realtime_setup = false;
+}
+
+void RayTracingRenderer::draw_dialog() {
+  ImGui::Text("RayTracing");
+  // if (m_render_realtime) {
+  //   ImGui::BeginDisabled(true);
+  // }
+  ImGui::Checkbox("Real time rendering", &m_render_realtime);
+  ImGui::InputInt("(RT) Number of threads", &m_realtime_n_threads);
+  ImGui::InputInt("(RT) Max Depth", &m_realtime_max_depth);
+  ImGui::InputInt("(RT) Samples Per Pixle", &m_realtime_samples_per_pixel);
+  // if (m_render_realtime) {
+  //   ImGui::EndDisabled();
+  // }
+  if (m_render_realtime) {
+    setup_realtime();
+  } else {
+    destroy_realtime();
+  }
+
+  if (!m_rendering && !m_render_realtime) {
+    if (ImGui::Button("Render Frame")) {
+      render_frame(a_scene);
+    }
+  }
+
+  if (!m_render_realtime) {
+    ImGui::InputInt("Number of threads", &m_n_threads);
+    ImGui::InputInt("Max Depth", &m_max_depth);
+    ImGui::InputInt("Samples Per Pixle", &m_samples_per_pixel);
+
+    if (m_has_render) {
+      if (ImGui::Button("Save to file")) {
+        save_to_file();
+      }
+      // ImGui::Begin("Rendered Frame");
+      // // m_texture.bind();
+      // ImGui::Image((ImTextureID)(intptr_t)m_texture.id,
+      //              ImVec2(m_texture.width, m_texture.height));
+      // ImGui::End();
+    }
+  }
+}
+void RayTracingRenderer::setup_threads(ThreadTask task, Scene const *scene) {
+  auto stepper = [](int height, int n_threads) -> std::vector<Bounds> {
+    std::vector<Bounds> ret;
+    int step = height / n_threads;
+    int cur_step = 0;
+    for (int i = 0; i < n_threads; i++) {
+      int start = cur_step;
+      int stop = cur_step + step - 1;
+      if (stop >= height) {
+        stop = height - 1;
+      }
+      // if it dosen't divide evenly get the rest
+      if (i == n_threads - 1 && stop < height) {
+        stop = height - 1;
+      }
+
+      ret.push_back({start, stop});
+      cur_step += step;
+    }
+
+    return ret;
+  };
+
+  int i = 0;
+  RenderTaskArgs ra{a_camera,
+                    m_image_height,
+                    m_image_width,
+                    m_samples_per_pixel,
+                    m_max_depth,
+                    0,
+                    0};
+
+  // thread function;
+  auto call = [this](ThreadTask tt, RenderTaskArgs ra, Scene const *s,
+                     int index) {
+    tt.index = index;
+    ren_task(tt, ra, s);
+    // m_thread_finished.at(index) = true;
+  };
+
+  // pre thread creation setup
+  if (task.type == ThreadTaskType::normal) {
+    m_threads.clear();
+  } else {
+    m_realtime_threads.clear();
+  }
+
+  // create the threads
+  int n_threads = 0;
+  if (task.type == ThreadTaskType::normal) {
+    n_threads = m_n_threads;
+  } else {
+    n_threads = m_realtime_n_threads;
+  }
+
+  for (auto const s : stepper(m_image_height, n_threads)) {
+    auto [start, stop] = s;
+    ra.start = start;
+    ra.stop = stop;
+    if (task.type == ThreadTaskType::normal) {
+      task.thread_finished = &m_thread_finished.at(i);
+      task.should_finish = nullptr;
+      task.should_pause = nullptr;
+
+      m_threads.emplace_back(call, task, ra, scene, i);
+    } else {
+      task.thread_finished = &m_realtime_thread_finished.at(i);
+      task.should_finish = &m_realtime_thread_should_stop.at(i);
+      task.should_pause = &m_should_pause;
+
+      m_realtime_threads.emplace_back(call, task, ra, scene, i);
+    }
+    i++;
+  }
+}
+
 void RayTracingRenderer::render_frame(Scene const *scene) {
-  Log::the().add_log("Starting Raytracing\n");
+  Log::the().add_log("Starting Render\n");
   Log::the().add_log("Width=%zu, Height=%zu\n", m_image_width, m_image_height);
   Log::the().add_log("Threads=%d\n", m_n_threads);
 
-  // Camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 90,
-  //            aspect_ratio);
   assert(a_camera);
   a_camera->update_rt_vectors();
 
   m_pixels.clear();
   m_pixels.resize(m_len);
 
-  m_threads.clear();
-  for (auto &f : m_thread_finished)
-    f = false;
-  std::size_t step = m_image_height / m_n_threads;
-  std::size_t cur_step = 0;
-  for (std::size_t i = 0; i < m_n_threads; i++) {
-    int start = cur_step;
-    int stop = cur_step + step - 1;
-    if (stop >= m_image_height) {
-      stop = m_image_height - 1;
-    }
-    // if it dosen't divide evenly get the rest
-    if (i == m_n_threads - 1 && stop < m_image_height) {
-      stop = m_image_height - 1;
-    }
-    RenderTaskArgs ra{
-        a_camera,    m_image_height, m_image_width, m_samples_per_pixel,
-        m_max_depth, start,          stop};
-    auto call = [this, i](RenderTaskArgs ra, Scene const *s, Pixels *pixels) {
-      ren_task(ra, s, pixels);
-      m_thread_finished.at(i) = true;
-    };
-    m_threads.emplace_back(call, ra, scene, &m_pixels);
-    cur_step += step;
-  }
+  setup_threads({ThreadTaskType::normal, &m_pixels, 0, nullptr, nullptr},
+                scene);
 
   m_rendering = true;
 }
@@ -214,16 +434,26 @@ void RayTracingRenderer::thread_cleanup() {
 }
 
 void RayTracingRenderer::create_image_data() {
-  // std::reverse(m_pixels.begin(), m_pixels.end());
   if (m_texture.m_is_valid) {
     m_texture.update_data(m_pixels, m_image_width, m_image_height);
   } else {
     m_texture.generate_from_data(m_pixels, m_image_width, m_image_height);
   }
 }
+
+void RayTracingRenderer::rt_create_image_data() {
+  if (m_realtime_texture.m_is_valid) {
+    m_realtime_texture.update_data(m_realtime_pixels, m_image_width,
+                                   m_image_height);
+  } else {
+    m_realtime_texture.generate_from_data(m_realtime_pixels, m_image_width,
+                                          m_image_height);
+  }
+}
 void RayTracingRenderer::save_to_file() {
   assert(m_has_render);
+  stbi_flip_vertically_on_write(true);
   stbi_write_png("./render.png", m_image_width, m_image_height, m_channels,
-                 m_pixels.data(), m_image_width * m_channels);
+                 m_realtime_pixels.data(), m_image_width * m_channels);
 }
 } // namespace ren
--- src/renderers/raytracing.hpp
+++ src/renderers/raytracing.hpp
@@ -14,7 +14,6 @@
 #include "../shader.hpp"
 #include "../texture.hpp"
 
-
 namespace ren {
 
 using Pixels = std::vector<uint8_t>;
@@ -22,9 +21,38 @@ using Pixels = std::vector<uint8_t>;
 class RayTracingRenderer final : public Renderer {
 public:
   RayTracingRenderer(std::filesystem::path root_dir);
-  ~RayTracingRenderer() { thread_cleanup(); };
+  ~RayTracingRenderer() {
+    thread_cleanup();
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    destroy_realtime();
+  };
   void render(const Scene &, Transformations const &, double ticks) override;
   void draw_dialog() override;
+
+  struct RenderTaskArgs {
+    std::shared_ptr<Camera> cam;
+    size_t image_height;
+    size_t image_width;
+    int samples_per_pixel;
+    int max_depth;
+    int start;
+    int stop;
+  };
+
+  enum ThreadTaskType {
+    normal,
+    realtime,
+  };
+  struct ThreadTask {
+    ThreadTaskType type;
+    Pixels *pixels;
+    int index;
+    std::atomic<bool> *thread_finished;
+    std::atomic<bool> *should_finish;
+    std::atomic<bool> *should_pause;
+  };
 
 private:
   Shader m_fstexture_shader{};
@@ -32,15 +60,25 @@ private:
   Shader m_material_shader;
   Shader m_solid_shader;
 
+  using Bounds = std::tuple<int, int>;
+  void setup_threads(ThreadTask, Scene const *);
+
   int m_n_threads{1};
   std::vector<std::thread> m_threads{};
   std::array<std::atomic<bool>, 16> m_thread_finished{};
   bool threads_finished() {
-    for (size_t i = 0; i < m_n_threads; ++i) {
-      if (!m_thread_finished.at(i))
-        return false;
-    }
-    return true;
+      for (size_t i = 0; i < m_n_threads; ++i) {
+        if (!m_thread_finished.at(i))
+          return false;
+      }
+      return true;
+  }
+  bool realtime_threads_finished() {
+      for (size_t i = 0; i < m_realtime_n_threads; ++i) {
+        if (!m_realtime_thread_finished.at(i))
+          return false;
+      }
+      return true;
   }
 
   double const aspect_ratio = 16.0 / 9.0;
@@ -56,15 +94,43 @@ private:
   Texture m_texture{};
 
   // auto R = cos(pi / 4);
-  Scene m_scene;
+  Scene const *a_scene;
   std::shared_ptr<Camera> a_camera;
 
   bool m_rendering{false};
   bool m_has_render{false};
 
+  bool m_realtime_setup{false};
+  void setup_realtime();
+  void destroy_realtime();
+  GLuint VBO, VAO, EBO;
+  GLuint m_framebuffer;
+  bool m_render_realtime{false};
+  int m_realtime_samples_per_pixel = 5;
+  int m_realtime_max_depth = 25;
+  // double buffer
+  Pixels m_realtime_pixels{};
+  Texture m_realtime_texture{};
+
+  int m_realtime_n_threads{1};
+  std::vector<std::thread> m_realtime_threads{};
+  // double buffer
+  std::array<std::atomic<bool>, 16> m_realtime_thread_finished{false};
+  std::array<std::atomic<bool>, 16> m_realtime_thread_should_stop{false};
+  std::atomic<bool> m_should_pause{false};
+  
+  void rt_pause(){m_should_pause = true;}
+  void rt_unpause(){
+    for (size_t i = 0; i < m_realtime_n_threads; i++) {
+      m_realtime_thread_finished.at(i) = false;
+    }
+    m_should_pause = false;
+  }
+
   void render_frame(Scene const *);
   bool is_render_done();
   void create_image_data();
+  void rt_create_image_data();
   void save_to_file();
   void thread_cleanup();
 };
